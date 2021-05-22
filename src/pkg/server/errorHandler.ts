b8e533e69dcd2d442845b9940549596e9adcb0f9
Eugene Brodsky <fupslot@gmail.com>
2021-05-22 15:57:31 +0000

    linting fix

--- src/pkg/server/errorHandler.ts
+++ src/pkg/server/errorHandler.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 
 export default function errorHandler(): ErrorRequestHandler {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   return (error: Error, req: Request, res: Response, next: NextFunction) => {
     if (error) {
       console.error(error);
