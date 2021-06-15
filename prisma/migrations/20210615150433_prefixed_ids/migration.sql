d752b2fcf311bbc07fd899a52617d2c76498bad2
Eugene Brodsky <fupslot@gmail.com>
2021-06-15 18:13:19 +0300

    prisma: improve database schema

---
+++ prisma/migrations/20210615150433_prefixed_ids/migration.sql
@@ -0,0 +1,156 @@
+/*
+  Warnings:
+
+  - The values [Web,Spa,Native] on the enum `AuthType` will be removed. If these variants are still used in the database, this will fail.
+  - The primary key for the `Invite` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `OAuth2Client` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `method` on the `OAuth2Client` table. All the data in the column will be lost.
+  - You are about to drop the column `server_id` on the `OAuth2Client` table. All the data in the column will be lost.
+  - The primary key for the `OAuth2Server` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Profile` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Site` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "AuthType_new" AS ENUM ('web', 'spa', 'native');
+ALTER TABLE "OAuth2Client" ALTER COLUMN "type" DROP DEFAULT;
+ALTER TABLE "OAuth2Client" ALTER COLUMN "type" TYPE "AuthType_new" USING ("type"::text::"AuthType_new");
+ALTER TYPE "AuthType" RENAME TO "AuthType_old";
+ALTER TYPE "AuthType_new" RENAME TO "AuthType";
+DROP TYPE "AuthType_old";
+ALTER TABLE "OAuth2Client" ALTER COLUMN "type" SET DEFAULT 'web';
+COMMIT;
+
+-- Enable the pgcrypto module
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- FUNCTION: public.generate_uid(integer, text)
+
+-- DROP FUNCTION public.generate_uid(integer, text);
+
+CREATE OR REPLACE FUNCTION public.generate_uid(
+	size integer DEFAULT 12,
+	prefix text DEFAULT 'uid_'::text)
+    RETURNS text
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+DECLARE
+characters TEXT := 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  bytes BYTEA := gen_random_bytes(size);
+  l INT := length(characters);
+  i INT := 0;
+  output TEXT := '';
+BEGIN
+  WHILE i < size LOOP
+    output := output || substr(characters, get_byte(bytes, i) % l + 1, 1);
+    i := i + 1;
+  END LOOP;
+  RETURN CONCAT(prefix, output);
+  END;
+$BODY$;
+
+ALTER FUNCTION public.generate_uid(integer, text)
+    OWNER TO postgres;
+
+-- DropForeignKey
+ALTER TABLE "Invite" DROP CONSTRAINT "Invite_site_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OAuth2Client" DROP CONSTRAINT "OAuth2Client_server_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OAuth2Client" DROP CONSTRAINT "OAuth2Client_site_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OAuth2Server" DROP CONSTRAINT "OAuth2Server_site_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "User" DROP CONSTRAINT "User_site_id_fkey";
+
+-- DropIndex
+DROP INDEX "OAuth2Client.site_id_server_id_client_id_index";
+
+-- AlterTable
+ALTER TABLE "Invite" DROP CONSTRAINT "Invite_pkey",
+ALTER COLUMN "id" SET DEFAULT (generate_uid(14, 'invite_')::TEXT),
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "site_id" SET DATA TYPE TEXT,
+ADD PRIMARY KEY ("id");
+DROP SEQUENCE "Invite_id_seq";
+
+-- AlterTable
+ALTER TABLE "OAuth2Client" DROP CONSTRAINT "OAuth2Client_pkey",
+DROP COLUMN "method",
+DROP COLUMN "server_id",
+ALTER COLUMN "type" SET DEFAULT E'web',
+ALTER COLUMN "site_id" SET DATA TYPE TEXT,
+ALTER COLUMN "id" SET DEFAULT (generate_uid(14, 'client_')::TEXT),
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD PRIMARY KEY ("id");
+DROP SEQUENCE "OAuth2Client_id_seq";
+
+-- AlterTable
+ALTER TABLE "OAuth2Server" DROP CONSTRAINT "OAuth2Server_pkey",
+ALTER COLUMN "site_id" SET DATA TYPE TEXT,
+ALTER COLUMN "alias" SET DATA TYPE TEXT,
+ALTER COLUMN "id" SET DEFAULT (generate_uid(14, 'auth_')::TEXT),
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD PRIMARY KEY ("id");
+DROP SEQUENCE "OAuth2Server_id_seq";
+
+-- AlterTable
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_pkey",
+ALTER COLUMN "id" SET DEFAULT (generate_uid(14, 'profile_')::TEXT),
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "user_id" SET DATA TYPE TEXT,
+ADD PRIMARY KEY ("id");
+DROP SEQUENCE "Profile_id_seq";
+
+-- AlterTable
+ALTER TABLE "Site" DROP CONSTRAINT "Site_pkey",
+ALTER COLUMN "id" SET DEFAULT (generate_uid(14, 'site_')::TEXT),
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD PRIMARY KEY ("id");
+DROP SEQUENCE "Site_id_seq";
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" SET DEFAULT (generate_uid(14, 'user_')::TEXT),
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "site_id" SET DATA TYPE TEXT,
+ADD PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- DropEnum
+DROP TYPE "AuthMethod";
+
+-- CreateIndex
+CREATE INDEX "OAuth2Client.site_id_client_id_index" ON "OAuth2Client"("site_id", "client_id");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD FOREIGN KEY ("site_id") REFERENCES "Site"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Invite" ADD FOREIGN KEY ("site_id") REFERENCES "Site"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OAuth2Server" ADD FOREIGN KEY ("site_id") REFERENCES "Site"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OAuth2Client" ADD FOREIGN KEY ("site_id") REFERENCES "Site"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,26 +1,48 @@
-// import { sites } from "./seeds/sites";
+import { createGravatarHash, randomAlphaDigit } from "../src/internal";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.site.create({
+  const local = await prisma.site.create({
     data: {
       display_name: "local",
       alias: "local",
-      oauthServers: {
+      users: {
         create: {
-          name: "default",
-          applications: {
+          display_name: "john doe",
+          username: "john.doe",
+          first_name: "john",
+          last_name: "doe",
+          email: "john.doe@example.com",
+          profile: {
             create: {
-              client_id: "ZeacaeM0Ouy6ieceiv1xa2roo5mohch8",
-              client_secret: "oojiebitaPhah3southikePeepaghah7xi5bi0iceeY1Inu7",
-              redirect_uris: ["http://local.dev/callback"],
-              grants: ["authorization_code"],
+              job_title: "laymen",
+              gravatar_avatar_hash: createGravatarHash("john.doe@example.com"),
             },
           },
         },
       },
+    },
+  });
+
+  await prisma.oAuth2Server.create({
+    data: {
+      alias: "default",
+      site_id: local.id,
+    },
+  });
+
+  await prisma.oAuth2Client.create({
+    data: {
+      client_id: randomAlphaDigit(24),
+      client_secret: randomAlphaDigit(32),
+      token_endpoint_auth_method: "client_secret_post",
+      redirect_uris: ["http://local.dev/callback"],
+      grant_types: ["authorization_code"],
+      response_types: ["code"],
+      client_name: "local",
+      site_id: local.id,
     },
   });
 }
