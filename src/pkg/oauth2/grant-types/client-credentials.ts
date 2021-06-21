6dc2b4394777650ba43f3e47d282817c399e5498
Eugene Brodsky <fupslot@gmail.com>
2021-06-21 12:48:02 +0000

    oauth: client credentials

--- src/pkg/oauth2/grant-types/client-credentials.ts
+++ src/pkg/oauth2/grant-types/client-credentials.ts
@@ -2,6 +2,7 @@ import { Response } from "express";
 import { OAuthRequest } from "../request";
 import { GrantType } from "./grant-type";
 import { TokenResponseError } from "../token/token-response-error";
+import { AccessTokenResponseParams, OAuthResponse } from "../response";
 
 export class ClientCredentialGrant extends GrantType {
   async handle(request: OAuthRequest, res: Response): Promise<void> {
@@ -11,6 +12,11 @@ export class ClientCredentialGrant extends GrantType {
      */
     const auth = request.authorization;
 
+    /**
+     * * REFACTOR: There are few places where the authorization credentials are
+     * *           required. Thinking about moving this part to be member of GrantType class
+     * * const auth = this.ensureAuthorizationCredentials(request); AuthToken | throw
+     */
     if (!auth || auth.type !== "basic" || !auth.user || !auth.password) {
       const responseError = new TokenResponseError("invalid_client");
       responseError.set(
@@ -37,6 +43,21 @@ export class ClientCredentialGrant extends GrantType {
       throw new TokenResponseError("unauthorized_client");
     }
 
-    return;
+    /**
+     * Refresh token SHOULD NOT be included for the client credentials grant
+     */
+
+    const tokenResponseParams: AccessTokenResponseParams = {
+      type: "token",
+      token_type: "bearer",
+      access_token: this.model.generateAccessToken(),
+      expires_in: client.access_token_lifetime,
+    };
+
+    const response = new OAuthResponse(request, tokenResponseParams);
+    res.set(response.headers);
+    res.json(response.body);
+
+    return Promise.resolve();
   }
 }
