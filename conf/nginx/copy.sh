385f5ab6c0e3c383c3d68fa2c84d169051d64ae3
Eugene Brodsky <fupslot@gmail.com>
2021-06-27 11:44:27 +0300

    turn on tls with custom CA certificate

--- conf/nginx/copy.sh
+++ conf/nginx/copy.sh
@@ -5,6 +5,10 @@ if [ ! -d /etc/nginx/example.net ]; then
   sudo mkdir -p /etc/nginx/example.net
 fi
 
+if [ ! -d /etc/nginx/tls ]; then
+  sudo mkdir -p /etc/nginx/tls/example.net
+fi
+
 sudo rm /etc/nginx/sites-enabled/*
 sudo rm /etc/nginx/sites-available/*
 sudo rm /etc/nginx/example.net/*
@@ -12,6 +16,9 @@ sudo rm /etc/nginx/example.net/*
 sudo cp -R /var/staw-backend/conf/nginx/example.net/* /etc/nginx/example.net/
 sudo cp -R /var/staw-backend/conf/nginx/sites-available/* /etc/nginx/sites-available/
 sudo cp /var/staw-backend/conf/nginx/nginx.conf /etc/nginx/nginx.conf
+
+# Copying TLS certificates
+sudo cp -R /var/staw-backend/conf/nginx/tls/example.net/* /etc/nginx/tls/example.net/
 
 # Link example.net
 sudo ln -s /etc/nginx/sites-available/debug.example.net /etc/nginx/sites-enabled/debug.example.net
